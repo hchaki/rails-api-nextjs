2ff964a4ee19e84b3a9c534b88274e0e55510298
hchaki <hchaki@users.noreply.local>
2026-01-05 00:46:28 +0900

    feat: create memo api を追加

--- backend/app/controllers/memos_controller.rb
+++ backend/app/controllers/memos_controller.rb
@@ -3,4 +3,20 @@ class MemosController < ApplicationController
     memos = Memo.all
     render json: memos
   end
+
+  def create
+    memo = Memo.new(memo_params)
+
+    if memo.save
+      render json: memo, status: :created
+    else
+      render json: { errors: memo.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def memo_params
+    params.require(:memo).permit(:title, :content)
+  end
 end
