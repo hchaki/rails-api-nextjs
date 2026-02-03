376fe3c58061720d54458cb487991ab32cc33a6b
hchaki <hchaki@users.noreply.local>
2026-02-03 11:30:24 +0900

    User モデルの作成

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_01_04_125534) do
+ActiveRecord::Schema[8.1].define(version: 2026_02_03_011546) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -19,5 +19,13 @@ ActiveRecord::Schema[8.1].define(version: 2026_01_04_125534) do
     t.datetime "created_at", null: false
     t.string "title"
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "email", null: false
+    t.string "password_digest", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 end
