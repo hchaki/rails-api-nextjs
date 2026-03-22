c16375ef505d80e418b7208942b5a6f723fd7885
hchaki <hchaki@users.noreply.local>
2026-03-22 18:20:46 +0900

    feat: backendにリフレッシュトークン機能を追加

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_02_03_011546) do
+ActiveRecord::Schema[8.1].define(version: 2026_03_22_021315) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -25,6 +25,8 @@ ActiveRecord::Schema[8.1].define(version: 2026_02_03_011546) do
     t.datetime "created_at", null: false
     t.string "email", null: false
     t.string "password_digest", null: false
+    t.string "refresh_token_digest"
+    t.datetime "refresh_token_expires_at"
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
