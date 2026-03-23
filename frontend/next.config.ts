71d53718e3ba9996135e41d9c6acbca91728c944
hchaki <hchaki@users.noreply.local>
2026-03-23 09:50:08 +0900

    BACKEND_URLを追加

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -6,7 +6,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: "http://api:3000/:path*",
+        destination: `${process.env.BACKEND_URL}/:path*`,
       },
     ];
   },
